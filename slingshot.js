dc32a9514457f982e37efcf1e424d88efb111b9d
daizygoyal <72451280+daizygoyal@users.noreply.github.com>
2020-12-31 14:59:23 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,31 @@
+class Slingshot{
+
+    constructor(bodyA,pointB){
+ 
+        var options={
+            bodyA :  bodyA,
+            pointB :  pointB,
+            length : 15,
+            Stiffness : 0.01
+    }
+             this.pointB=pointB;
+             this.sling=Constraint.create(options);
+            World.add(world,this.sling);
+    }
+
+    fly(){
+
+        this.sling.bodyA=null
+    }
+    display(){
+    if(this.sling.bodyA){
+
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+             strokeWeight(4);  
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+    
+    }
+    
+    }
+}
